96adb64b3316b966f9cce32d55987bdc8f3d3c74
fastgameplay <61421145+fastgameplay@users.noreply.github.com>
2021-10-13 14:52:00 +0400

    Update Point.kt

--- src/main/kotlin/Point.kt
+++ src/main/kotlin/Point.kt
@@ -1,54 +1,54 @@
-class Point(){
+class Point{
     private var x: Float = 0.0f
     private var y: Float = 0.0f
 
     //region Get X/Y()
-    public fun GetX() : Float{
+    fun GetX() : Float{
         return x
     }
-    public fun GetY() : Float{
+    fun GetY() : Float{
         return y
     }
     //endregion
 
     //region SetPoints()
-    public fun SetPoints(_x: Int, _y: Int){
+    fun SetPoints(_x: Int, _y: Int){
         x = _x.toFloat()
         y = _y.toFloat()
     }
-    public fun SetPoints(_x: Float, _y: Float){
+    fun SetPoints(_x: Float, _y: Float){
         x = _x
         y = _y
     }
-    public fun SetPoints(_x: Double, _y: Double){
+    fun SetPoints(_x: Double, _y: Double){
         x = _x.toFloat()
         y = _y.toFloat()
     }
     //endregion
 
     //region AddPoints()
-    public fun AddPoints(_x: Int, _y: Int){
+    fun AddPoints(_x: Int, _y: Int){
         x = _x.toFloat()
         y = _y.toFloat()
     }
-    public fun AddPoints(_x: Float, _y: Float){
+    fun AddPoints(_x: Float, _y: Float){
         x = _x
         y = _y
     }
-    public fun AddPoints(_x: Double, _y: Double){
+    fun AddPoints(_x: Double, _y: Double){
         x = _x.toFloat()
         y = _y.toFloat()
     }
     //endregion
 
     //region IsEqual()
-    public fun EqualTo(other : Point ) : Boolean{
+    fun EqualTo(other : Point ) : Boolean{
         if(other.GetX() == this.x && other.GetY() == this.y)
             return true
         else
             return false
     }
-    public fun EqualTo(_x : Float , _y: Float) : Boolean{
+    fun EqualTo(_x : Float , _y: Float) : Boolean{
         if(_x == this.x && _y == this.y) return true
         else return false
     }
